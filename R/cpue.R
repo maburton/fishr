15582acb9c8930836aeb895f681929da57211f01
maburton <Meghan.Burton@dfo-mpo.gc.ca>
2026-02-24 10:08:57 -0800

    End of session 1 committing all files

--- R/cpue.R
+++ R/cpue.R
@@ -1,3 +1,15 @@
+#' Calculate Catch per Unit Effort (CPUE)
+#'
+#' @param catch Numeric vector of catch
+#' @param effort Numeric vector of effort
+#' @param gear_factor integer adjustment for gear standardization
+#'
+#' @return A numeric vector of CPUE values
+#' @export
+#'
+#' @examples
+#' cpue (100, 10)
+#' cpue (100, 10, gear_factor = 0.5)
 cpue <- function(catch,
                  effort,
                  gear_factor = 1){
